3b3a87ea91a0a97244fb992cb3d42f321684c656
Colt <coltsteele1@gmail.com>
2015-03-18 09:33:55 -0700

    clean up

--- app.js
+++ app.js
@@ -8,8 +8,4 @@ demoApp.controller('SimpleController', function($scope) {
 	  $scope.choices.push({'id':'choice'+newItemNo});
 	};
 
-	$scope.showAddChoice = function(choice) {
-  	return choice.id === $scope.choices[$scope.choices.length-1].id;
-	};
-
 });
